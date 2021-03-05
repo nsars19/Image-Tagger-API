55ec56c5cec4d5bf392b749a1e01dd708bb887e9
Nick Sardonini <nsardonini@gmail.com>
2021-03-05 17:28:57 -0500

    add create route & create controller method

--- app/controllers/highscores_controller.rb
+++ app/controllers/highscores_controller.rb
@@ -1,9 +1,23 @@
 class HighscoresController < ApplicationController
   def show
-    @times = Highscore.where(level_id: params[:level_id])
+    @times = Highscore.where(level_id: params[:id])
                       .order(time: :asc)
                       .limit(10)
 
     render json: @times
+  end
+
+  def create
+    @time = Highscore.create(
+      name: params[:name],
+      time: params[:time],
+      level_id: params[:level_id]
+    )
+  end
+
+  private
+
+  def highscore_params
+    params.require(:highscore).permit(:name, :time, :level_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :highscores, only: :show
+  resources :highscores, only: [:show, :create]
   resources :levels, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
